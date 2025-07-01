df8db6cbabbb16173eb09fba8257d51490526323
Deepanshu Monga <mongadeepanshu104@gmail.com>
2025-07-01 18:21:35 +0530

    Update catdogclassifier.py

--- CatDogClassifier/catdogclassifier.py
+++ CatDogClassifier/catdogclassifier.py
@@ -6,16 +6,16 @@ import time
 import os
 import gdown
 MODEL_PATH = "CatsvsDogs.h5"
-FILE_ID = "1h7BiKqgVuL3_RPuBzJKXDCf0K3BTnvaK"  # Replace this!
+FILE_ID = "1h7BiKqgVuL3_RPuBzJKXDCf0K3BTnvaK"
 if not os.path.exists(MODEL_PATH):
     with st.status("🚀 Initializing download...", expanded=True) as status:
-    st.write("🔁 Initializing secure model download...")
-    time.sleep(1)
-    st.write("⬇️ Fetching trained model...")
-    gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", MODEL_PATH, quiet=False)
-    time.sleep(1)
-    st.write("✅ Model downloaded successfully!")
-    status.update(label="✅ All set!", state="complete")
+        st.write("🔁 Initializing secure model download...")
+        time.sleep(1)
+        st.write("⬇️ Fetching trained model...")
+        gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", MODEL_PATH, quiet=False)
+        time.sleep(1)
+        st.write("✅ Model downloaded successfully!")
+        status.update(label="✅ All set!", state="complete")
 model = load_model(MODEL_PATH)
 st.title(":orange[Welcome to Cat vs Dog CLassifier System : ]")
 st.subheader("Upload image here :")
@@ -35,8 +35,8 @@ if(Image!=None):
         st.write("✅ Prediction complete!")
         time.sleep(1)
     img = image.load_img(Image, target_size=(150, 150))
-    img_array = image.img_to_array(img) / 255.0  # Normalize
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
     prediction = model.predict(img_array)[0][0]
     if prediction > 0.5:
         st.subheader(f"Output = Prediction: Dog 🐶 ({prediction * 100:.2f}% confidence)")
