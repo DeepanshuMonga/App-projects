059a4f3e17b7ec3bbef07a9d7fa20e18ef4b7d12
Deepanshu Monga <mongadeepanshu104@gmail.com>
2025-07-15 17:57:41 +0530

    Update emotion.py

--- EmotionDetector/emotion.py
+++ EmotionDetector/emotion.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import time
 import cv2
 from PIL import Image
